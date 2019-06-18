fcaacc7ffbdc52df27566a99b78b5754777e7b9c
AlbaBisante <alba.bisante@icloud.com>
2019-06-18 17:56:38 +0200

    Food, food_rest aggiunti (copiati da forna), non funziona il collegamento con bottone menu

--- app/src/main/java/com/example/myapplication/res_detail.java
+++ app/src/main/java/com/example/myapplication/res_detail.java
@@ -1,10 +1,13 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Button;
 
 import com.example.myapplication.db_obj.Restaurant;
 
@@ -18,9 +21,20 @@ public class res_detail extends AppCompatActivity {
         setSupportActionBar(toolbar);
         //getSupportActionBar().setTitle("SeatEat");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        Restaurant rist = (Restaurant) getIntent().getSerializableExtra("Restaurant");
+        final Restaurant rist = (Restaurant) getIntent().getSerializableExtra("Restaurant");
         System.out.println(rist.getRESTAURANT_TITLE());
         TextView nome_res = findViewById(R.id.res_name);
+
+        Button button = findViewById(R.id.menu_button);
+         button.setOnClickListener(new View.OnClickListener() {
+             public void onClick(View v) {
+                 System.out.println("hai clikkato MENU");
+                 Intent intent = new Intent(res_detail.this,food_rest.class);
+                 intent.putExtra("Restaurant",rist); // passo l'oggetto ristornate
+                 startActivity(intent);
+             }
+         });
+
         nome_res.setText(rist.getRESTAURANT_TITLE());
         RatingBar ratingBar = findViewById(R.id.ratingBar2);
         ratingBar.setEnabled(false);
