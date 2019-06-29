a863906a6645a419b78f7805e8ed5e7586b1ead8
francesco fornasieri <fornasieri.1692740@studenti.uniroma1.it>
2019-06-30 00:10:49 +0200

    login news

--- app/src/main/java/com/example/myapplication/Settings.java
+++ app/src/main/java/com/example/myapplication/Settings.java
@@ -3,9 +3,16 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class Settings extends AppCompatActivity {
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +24,40 @@ public class Settings extends AppCompatActivity {
         getSupportActionBar().setTitle("Settings");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        Button logout_btn = findViewById(R.id.logout_btn);
+        logout_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                preferences = getSharedPreferences("loginref", MODE_PRIVATE);
+
+                editor = preferences.edit();
+                editor.putString("nome", null);
+                editor.putBoolean("savelogin", false);
+                editor.putString("password",null);
+                editor.commit();
+                Intent i = new Intent(Settings.this, Login.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        } else if (id == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
