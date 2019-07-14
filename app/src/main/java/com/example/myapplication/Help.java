10c86bd63da38fa6c04bf818f8ff1440d4bf432c
francesco fornasieri <fornasieri.1692740@studenti.uniroma1.it>
2019-07-15 00:27:47 +0200

    update fooddetail

--- app/src/main/java/com/example/myapplication/Help.java
+++ app/src/main/java/com/example/myapplication/Help.java
@@ -1,22 +1,29 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-public class Help extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class Help extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
-        Toolbar toolbar = findViewById(R.id.include);
+        Toolbar toolbar = findViewById(R.id.tool_bar_simple);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -37,4 +44,33 @@ public class Help extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        System.out.println("uauuuu");
+
+        if (id == R.id.nav_home) {
+            // Handle the camera action
+
+        }  else if (id == R.id.nav_help) {
+            Intent intent = new Intent(getApplicationContext(), Help.class);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        else if (id == R.id.nav_settings){
+            Intent intent = new Intent(getApplicationContext(), Settings.class);
+            startActivity(intent);
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout_help);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
