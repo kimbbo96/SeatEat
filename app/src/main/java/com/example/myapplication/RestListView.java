b7e1076269f16e955f3980683a2ce5249338b3c1
francesco fornasieri <fornasieri.1692740@studenti.uniroma1.it>
2019-10-01 19:37:21 +0200

    onresume

--- app/src/main/java/com/example/myapplication/RestListView.java
+++ app/src/main/java/com/example/myapplication/RestListView.java
@@ -51,13 +51,8 @@ public class RestListView extends ArrayAdapter<String> {
         Float[] rate = new Float[restaurants.length];
         Float[] distance = new Float[restaurants.length];
 
-        LocationManager lm = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions((Activity) getContext(), new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-        } else {
-            MainActivity.here = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        }
+
+        System.out.println(MainActivity.here+"quaaa");
         Location targetLocation = new Location("");
 
         for (int i = 0; i < restaurants.length; i++) {
